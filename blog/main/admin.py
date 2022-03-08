34ef6ca79b191d75464a5c794d4d151f9c951a57
sirdesmond09 <sirdesmond09@gmail.com>
2022-03-08 15:37:47 +0100

    models

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Employee
 # Register your models here.
+
+# admin.site.register(Employee)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "is_active", "date_joined"]
+    list_editable = ["is_active"]
+    list_filter = ["is_active", "date_joined"]
--- blog/main/models.py
+++ blog/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    job_desc = models.TextField()
+    age  = models.IntegerField()
+    img = models.ImageField(null=True, blank=True)
+    salary = models.FloatField()
+    date_joined = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.email
+    
+    
+    
