ffab11df03e17a87ed14b6ddb54f8c5539cafe85
sirdesmond09 <sirdesmond09@gmail.com>
2022-03-08 12:23:29 +0100

    static and urls

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path("", views.home_page, name="homepage"),
-   path("about/", views.about_page, name="aboutpage") 
+   path("about/", views.about_page, name="aboutpage"), 
+   path("about/<int:name>/boys/<int:age>", views.about_pages) 
 ]
--- blog/main/views.py
+++ blog/main/views.py
@@ -5,9 +5,30 @@ def home_page(request):
     if request.method == "POST":
         print(request.POST)
         return redirect("aboutpage")
-    return render(request, "home.html")
+    
+    context = {
+        "section" : "home"
+    }
+    
+    return render(request, "home.html", context=context)
 
 
 def about_page(request):
     
-    return render(request, "about.html")
+    context = {
+        "section" : "about",
+    }
+    
+    return render(request, "about.html", context=context)
+
+
+def about_pages(request, name, age):
+    
+    name = name+age
+    
+    context = {
+        "section" : "about",
+        "name" : name
+    }
+    
+    return render(request, "about.html", context=context)
